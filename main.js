c0467310f9c6193fe7aaf48bdc9f64e7523392ed
Hermerson Araújo <ne.hermerson@gmail.com>
2020-10-13 17:38:33 -0300

    feat: add computed properties

--- main.js
+++ main.js
@@ -3,22 +3,24 @@ const app = Vue.createApp({
     return {
       cart: 0,
       product: 'Socks',
+      brand: 'Vue Mastery',
       description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Non corporis necessitatibus aliquid ex veritatis amet numquam fuga atque voluptatibus, quaerat tempora culpa voluptates eveniet earum dolore pariatur nam corrupti impedit.',
-      image: './assets/images/socks_green.jpg',
       link: 'https://github.com/hdev14',
-      inStock: false,
+      selectedVariant: 0,
       onSale: true,
       details: ['50% cotton', '30% wool', '20% polyester'],
       variants: [
         {
           id: 2234,
           color: 'green',
-          image: './assets/images/socks_green.jpg'
+          image: './assets/images/socks_green.jpg',
+          quantity: 50
         },
         {
           id: 2235,
           color: 'blue',
-          image: './assets/images/socks_blue.jpg'
+          image: './assets/images/socks_blue.jpg',
+          quantity: 0
         }
       ],
       sizes: [22, 33, 44]
@@ -35,8 +37,26 @@ const app = Vue.createApp({
         this.cart--
     },
 
-    updateImage(variantImage) {
-      this.image = variantImage
+    updateVariant(index) {
+      this.selectedVariant = index
+    }
+  },
+
+  computed: {
+    title() {
+      return `${this.brand} ${this.product}`
+    },
+
+    image() {
+      return this.variants[this.selectedVariant].image
+    },
+
+    inStock() {
+      return this.variants[this.selectedVariant].quantity
+    },
+
+    onSaleMessage() {
+      return this.onSale && `${this.brand} ${this.product} is on sale.`
     }
   }
 })
