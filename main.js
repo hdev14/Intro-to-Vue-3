10899b80c474afcd704734ec542077e8a8b84375
Hermerson Araújo <ne.hermerson@gmail.com>
2020-10-13 16:38:49 -0300

    feat: add inventory and onSale conditions

--- main.js
+++ main.js
@@ -4,7 +4,9 @@ const app = Vue.createApp({
       product: 'Socks',
       description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Non corporis necessitatibus aliquid ex veritatis amet numquam fuga atque voluptatibus, quaerat tempora culpa voluptates eveniet earum dolore pariatur nam corrupti impedit.',
       image: './assets/images/socks_green.jpg',
-      link: 'https://github.com/hdev14'
+      link: 'https://github.com/hdev14',
+      inventory: 100,
+      onSale: true
     }
   }
 })
