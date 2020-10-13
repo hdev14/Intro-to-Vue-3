770aa6f26f11a6a9b0ce9c6e815c5181154b28c7
Hermerson Araújo <ne.hermerson@gmail.com>
2020-10-13 16:18:14 -0300

    feat: create and mount vue app

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-const product = 'Socks'
+const app = Vue.createApp({
+  data () {
+    return {
+      product: 'Socks',
+      description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Non corporis necessitatibus aliquid ex veritatis amet numquam fuga atque voluptatibus, quaerat tempora culpa voluptates eveniet earum dolore pariatur nam corrupti impedit.'
+    }
+  }
+})
