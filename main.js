1118f0f5fa4eb6a8e03c2db2ce993125a9b4e5cd
Hermerson Araújo <ne.hermerson@gmail.com>
2020-10-13 16:28:33 -0300

    feat: add product image

--- main.js
+++ main.js
@@ -2,7 +2,9 @@ const app = Vue.createApp({
   data () {
     return {
       product: 'Socks',
-      description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Non corporis necessitatibus aliquid ex veritatis amet numquam fuga atque voluptatibus, quaerat tempora culpa voluptates eveniet earum dolore pariatur nam corrupti impedit.'
+      description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Non corporis necessitatibus aliquid ex veritatis amet numquam fuga atque voluptatibus, quaerat tempora culpa voluptates eveniet earum dolore pariatur nam corrupti impedit.',
+      image: './assets/images/socks_green.jpg',
+      link: 'https://github.com/hdev14'
     }
   }
 })
