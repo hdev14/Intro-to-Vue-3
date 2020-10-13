15399614bee5ef536da783004ef471e6e1804c66
Hermerson Araújo <ne.hermerson@gmail.com>
2020-10-13 17:02:12 -0300

    feat: create methods to improve ux

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const app = Vue.createApp({
   data () {
     return {
+      cart: 0,
       product: 'Socks',
       description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Non corporis necessitatibus aliquid ex veritatis amet numquam fuga atque voluptatibus, quaerat tempora culpa voluptates eveniet earum dolore pariatur nam corrupti impedit.',
       image: './assets/images/socks_green.jpg',
@@ -9,10 +10,32 @@ const app = Vue.createApp({
       onSale: true,
       details: ['50% cotton', '30% wool', '20% polyester'],
       variants: [
-        { id: 2234, color: 'green' },
-        { id: 2234, color: 'blue' }
+        {
+          id: 2234,
+          color: 'green',
+          image: './assets/images/socks_green.jpg'
+        },
+        {
+          id: 2235,
+          color: 'blue',
+          image: './assets/images/socks_blue.jpg'
+        }
       ],
       sizes: [22, 33, 44]
+    }
+  },
+
+  methods: {
+    addToCart() {
+      this.cart++
+    },
+
+    removeFromCart() {
+      this.cart--
+    },
+
+    updateImage(variantImage) {
+      this.image = variantImage
     }
   }
 })
